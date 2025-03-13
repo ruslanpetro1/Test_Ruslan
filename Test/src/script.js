89a58b094320c26b89b5b1c5927be49a68d77781
ruslanpetro1 <96756469+ruslanpetro1@users.noreply.github.com>
2025-03-13 17:11:29 +0200

    Delete Test directory

--- Test/src/script.js
+++
@@ -1,7 +0,0 @@
-var _a;
-// Ваш TypeScript код
-(_a = document.getElementById("teamForm")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (e) {
-    e.preventDefault();
-    // Додатковий код обробки форми
-    console.log("Form submitted!");
-});
--- Test/src/script.ts
+++
@@ -1,7 +0,0 @@
-// Ваш TypeScript код
-document.getElementById("teamForm")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    // Додатковий код обробки форми
-    console.log("Form submitted!");
-});
