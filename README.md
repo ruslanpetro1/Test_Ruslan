c6025a247d51f07644229ada890c1ec76033e759
ruslanpetro1 <96756469+ruslanpetro1@users.noreply.github.com>
2025-03-13 17:11:56 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Test_Ruslan
+"# Test" 
---
+++ src/script.js
@@ -0,0 +1,7 @@
+var _a;
+// Ваш TypeScript код
+(_a = document.getElementById("teamForm")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (e) {
+    e.preventDefault();
+    // Додатковий код обробки форми
+    console.log("Form submitted!");
+});
